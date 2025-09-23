814c718bb0372880beb7f42db8855ecd54920bf3
official-adebanjo <adebanjo.adeniji@concept-nova.com>
2025-09-23 09:30:54 +0100

    replace static content with dynamic changing format

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="py-10 bg-gray-900 text-white text-center">
-      <p>© 2025 NLWC Ikorodu Beach Hangout. All rights reserved.</p>
+      <p>© {currentYear} NLWC Ikorodu Beach Hangout. All rights reserved.</p>
     </footer>
   );
 };
